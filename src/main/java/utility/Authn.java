461518f1720ce55c93624fa14910b3bf3a5f35a6
paragpatil7@gmail.com <paragpatil7@gmail.com>
2022-07-08 16:51:20 +0530

    New Framework RestAssured

---
+++ src/main/java/utility/Authn.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class Authn {
+	
+	public final static String bearer_token = "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";
+	
+	public static String getBearerToken() {
+		return bearer_token;
+	}
+
+}
